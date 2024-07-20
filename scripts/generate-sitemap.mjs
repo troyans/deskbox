20d10132a8bb997fcc75a161b5039a319cc0e994
troyans <anwar.azulfa@gmail.com>
2024-07-20 11:52:11 +0700

    change generate sitemap

--- scripts/generate-sitemap.mjs
+++ scripts/generate-sitemap.mjs
@@ -16,7 +16,7 @@ async function generate() {
 
   const sitemap = `
     <?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmln="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
         ${pages
           .map((page) => {
             const path = page
