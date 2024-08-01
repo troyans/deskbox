645af36b1166a2e967ce42fca50ecc87655cbb4a
troyans <anwar.azulfa@gmail.com>
2024-08-01 09:53:24 +0700

    theme config

--- theme.config.js
+++ theme.config.js
@@ -1,9 +1,36 @@
 export default {
-  logo: <span>Deskbox</span>,
+  
+  
+
   project: {
     link: "https://deskbox.co",
   },
-  feedback: false,
+  feedback: true,
   editLink: false,
+
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – SWR'
+    }
+  },
+
+  logo: (
+    <>
+      <svg style={{marginTop:'0.2em'}} width="20" height="20" viewBox="0 0 340 340" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="170" cy="180" r="150" stroke="#5423E7" stroke-width="40" stroke-linecap="round" stroke-linejoin="bevel" stroke-dasharray="50 50"/>
+      </svg>
+
+      <span style={{ marginLeft: '.2em', marginTop:'0', fontWeight: 800, fontSize:24, color:"#5423E7" }}>
+        Deskbox
+      </span>
+    </>
+  ),
+
+  
+
+  
+
+
+
   // ... other theme options
 };
