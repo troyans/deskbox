8fa7a5aa3a35225b207f5617cfa2c0edd8de0c05
troyans <anwar.azulfa@gmail.com>
2024-09-04 16:35:40 +0700

    add cta

--- components/landingPage/landingHero.tsx
+++ components/landingPage/landingHero.tsx
@@ -66,7 +66,7 @@ export default function LandingHero() {
                         <h1 className="mt-4 text-4xl font-bold text-black lg:mt-8 sm:text-6xl xl:text-8xl">Chatbot For Your Business</h1>
                         <p className="mt-4 text-base text-black lg:mt-8 sm:text-xl">Streamline workflow and increase your team productivity with AI-powered chatbots in your business such as customer support, internal knowledge base, lead generation, etc</p>
     
-                        <a href="#cta" title="" className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-black transition-all duration-200 bg-yellow-300 rounded-full lg:mt-16 hover:bg-yellow-400 focus:bg-yellow-400" role="button">
+                        <a href="#cta" title="" className="inline-flex items-center px-6 py-4 mt-8 font-semibold text-white transition-all duration-200 bg-[#5423E7] rounded-full lg:mt-16 hover:bg-black hover:text-white focus:bg-black" role="button">
                             Join waiting list
                             <svg className="w-6 h-6 ml-8 -mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M13 9l3 3m0 0l-3 3m3-3H8m13 0a9 9 0 11-18 0 9 9 0 0118 0z" />
